a084d9ed401c5bdd4dc1048737957c4246b1ae3c
HiraiMomo-TWICE <caogiahung11A2@gmail.com>
2020-08-24 21:15:19 +0700

    new changes

--- Wumpus/SOURCE/TEST_TK/test_tk.py
+++ Wumpus/SOURCE/TEST_TK/test_tk.py
@@ -1,8 +1,8 @@
 from tkinter import *
-from PIL import ImageTk, Image
+# from PIL import ImageTk, Image
 
-windows = Tk(classname = 'Test tKinter')  # Create GUI
-windos.title('Wumpus-Agent')
+windows = Tk()  # Create GUI
+windows.title('Wumpus-Agent')
 windows.geometry("600x600")  # Change resolution
 
 quit_button = Button(windows, text = 'EXIT', command = windows.quit)
