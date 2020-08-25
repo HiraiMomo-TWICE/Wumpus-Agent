a4bab9ed257b1092701ab9a064e19d824b88098d
HiraiMomo-TWICE <caogiahung11A2@gmail.com>
2020-08-25 23:19:09 +0700

    Update WumpusGUI.py

--- Wumpus/SOURCE/TEST_TK/WumpusGUI.py
+++ Wumpus/SOURCE/TEST_TK/WumpusGUI.py
@@ -5,34 +5,7 @@ try:
 except ImportError:
     import Tkinter as tk     # python 2
     import tkFont as tkfont  # python 2
-
-class Layout(tk.Tk):
-    def __init__(self,master):
-       self.master = master
-       self.rootgeometry()
-       self.canvas = tk.Canvas(self.master)
-       self.canvas.pack()
-       self.background_image = Image.open('../../ASSET/background.jpg') 
-       self.image_copy = self.background_image.copy()
-       self.background = ImageTk.PhotoImage(self.background_image)
-       self.loadbackground()
-
-    def loadbackground(self):
-        self.label = tk.Label(self.canvas, image = self.background)
-        self.label.bind('<Configure>',self.resizeimage)
-        self.label.pack(fill='both', expand='yes')
-
-    def rootgeometry(self):
-       x=int(self.master.winfo_screenwidth()*0.7)
-       y=int(self.master.winfo_screenheight()*0.7)
-       z = str(x) + 'x' + str(y)
-       self.master.geometry(z)
-
-    def resizeimage(self,event):
-       image = self.image_copy.resize((self.master.winfo_width(),self.master.winfo_height()))
-       self.image1 = ImageTk.PhotoImage(image)
-       self.label.config(image = self.image1)
-
+    
 class WumpusWorld(tk.Tk):
 
     def __init__(self, *args, **kwargs):
@@ -41,7 +14,7 @@ class WumpusWorld(tk.Tk):
         self.iconphoto(False, ImageTk.PhotoImage(Image.open('../../ASSET/Wumpus-Icon.png')))
 
         self.title('WUMPUS WORLD AGENT')
-        self.title_font = tkfont.Font(family='Helvetica', size=50, weight="bold")
+        self.title_font = tkfont.Font(family='Helvetica', size=25, weight="bold")
 
         container = tk.Frame(self)
         container.pack(side = "top", fill = "both", expand = True)
@@ -49,7 +22,7 @@ class WumpusWorld(tk.Tk):
         container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
-        for each_frame in (StartMenu, MapSelection):
+        for each_frame in (StartMenu, MapSelection, Map1, Map2, Map3,):
             page_name = each_frame.__name__
             frame = each_frame(parent = container, controller = self)
             self.frames[page_name] = frame
@@ -84,9 +57,42 @@ class MapSelection(tk.Frame):
         label = tk.Label(self, text = "SELECT YOUR MAP", font=controller.title_font)
         label.pack(side = "top", fill = "x", pady = 10)
 
+        Map1Button = tk.Button(self, text = "MAP 1", command = lambda: controller.show_frame("Map1"))
+        Map1Button.pack()
+        Map2Button = tk.Button(self, text = "MAP 2", command = lambda: controller.show_frame("Map2"))
+        Map2Button.pack()
+        Map3Button = tk.Button(self, text = "MAP 3", command = lambda: controller.show_frame("Map3"))
+        Map3Button.pack()
+        BackButton = tk.Button(self, text = "BACK", command = lambda: controller.show_frame("StartMenu"))
+        BackButton.pack(side = "bottom")
+
+class Map1(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+
+        StartMenuButton = tk.Button(self, text = "BACK TO START MENU", command = lambda:controller.show_frame("StartMenu"))
+        StartMenuButton.pack()
+
+class Map2(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+
+        StartMenuButton = tk.Button(self, text = "BACK TO START MENU", command = lambda:controller.show_frame("StartMenu"))
+        StartMenuButton.pack()
+
+class Map3(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+
+        StartMenuButton = tk.Button(self, text = "BACK TO START MENU", command = lambda:controller.show_frame("StartMenu"))
+        StartMenuButton.pack()
+
 if __name__ == "__main__":
     wumpus = WumpusWorld()
-    a = Layout(wumpus)
     wumpus.mainloop()
-        
-        
