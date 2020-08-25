39b153ae0d233bce15f17c1b0dd6a8ed6d622051
HiraiMomo-TWICE <caogiahung11A2@gmail.com>
2020-08-25 12:14:00 +0700

    add Game Icon

--- Wumpus/SOURCE/TEST_TK/test_tk.py
+++ Wumpus/SOURCE/TEST_TK/test_tk.py
@@ -2,9 +2,13 @@ from tkinter import *
 from PIL import ImageTk, Image
 
 root = Tk()  # Create GUI
-root.title('Wumpus-Agent')
+root.title('Wumpus-Agent') # Game Title
 root.geometry("1280x720")  # Change resolution
 
+# Game Icon
+root.iconphoto(False, ImageTk.PhotoImage(Image.open('../../ASSET/Wumpus-Icon.png')))
+
+# Background Image for Wumpus
 C = Canvas(root, bg='blue', height=700, width=1280)
 imagefile = ImageTk.PhotoImage(Image.open('../../ASSET/background.jpg'))
 background_label = Label(root, image = imagefile)
