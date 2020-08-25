b48cd5c4ace7fc2cb0d61a4a9b16d3289f82b8fc
HiraiMomo-TWICE <caogiahung11A2@gmail.com>
2020-08-26 00:06:47 +0700

    Update main.py

--- Wumpus/SOURCE/TEST_TK/main.py
+++ Wumpus/SOURCE/TEST_TK/main.py
@@ -3,8 +3,12 @@ from tkinter import *
 from PIL import ImageTk, Image
 from sys import agv
 
+# Path Settings
+os.chdir('../')
+PATH = os.getcwd()
+
 if __name__ == '__main__':
-    filename = os.chdir(sys.agv[1])
+    filename = os.path.join(PATH, 'INPUT/MAP', arg[1])
 
     map_size, maze = handlein.read_file(filename)
 
