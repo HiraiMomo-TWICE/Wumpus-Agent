894ff898a7d7859f94c73267af67d28cbd1480f1
HiraiMomo-TWICE <caogiahung11A2@gmail.com>
2020-08-28 16:28:34 +0700

    update

--- Wumpus/SOURCE/TEST_TK/main.py
+++ Wumpus/SOURCE/TEST_TK/main.py
@@ -4,47 +4,56 @@ from PIL import ImageTk, Image
 from sys import argv
 
 def DrawMap(canvas, maze, maze_size, asset_path):
-    for each in maze:
-        for room in each:
-            if room == '-':
-                image = ImageTk.PhotoImage(Image.open(asset_path + 'terrain.png'))
-                canvas.create_image((10, 10), anchor = 'nw', image=image)
-            elif room == 'A':
-                image = ImageTk.PhotoImage(Image.open(asset_path + 'agent_right.png'))
-                canvas.create_image((10, 10), anchor = 'nw',image=image)
-            elif room == 'P':
-                image = ImageTk.PhotoImage(Image.open(asset_path + 'pit.png'))
-                canvas.create_image((10, 10), anchor = 'nw', image=image)
-            elif room == 'W':
-                image = ImageTk.PhotoImage(Image.open(asset_path + 'wumpus.png'))
-                canvas.create_image((10, 10), anchor = 'nw', image=image)
-            elif room == 'B':
-                image = ImageTk.PhotoImage(Image.open(asset_path + 'terrain_breeze.png'))
-                canvas.create_image((10, 10), anchor = 'nw', image=image)
-            elif room == 'S':
-                image = ImageTk.PhotoImage(Image.open(asset_path + 'terrain_stench.png'))
-                canvas.create_image((10, 10), anchor = 'nw', image=image)
-            elif room == 'BS':
-                image = ImageTk.PhotoImage(Image.open(asset_path + 'terrain_BS.png'))
-                canvas.create_image((10, 10), anchor = 'nw', image=image)
+
+    background = {
+        '-'     : ImageTk.PhotoImage(file=asset_path + '/terrain.png'),
+        'P'     : ImageTk.PhotoImage(file=asset_path + '/pit.png'),
+        'B'     : ImageTk.PhotoImage(file=asset_path + '/terrain_breeze.png'),
+        'S'     : ImageTk.PhotoImage(file=asset_path + '/terrain_stench.png'),
+        'BS'    : ImageTk.PhotoImage(file=asset_path + '/terrain_BS.png'),
+        'A'     : ImageTk.PhotoImage(file=asset_path + '/agent_right.png'),
+        'W'     : ImageTk.PhotoImage(file=asset_path + '/wumpus.png'),
+        'G'     : ImageTk.PhotoImage(file=asset_path + '/gold.png'),
+        'BG'    : ImageTk.PhotoImage(file=asset_path + '/gold_BG.png'),
+        'GS'    : ImageTk.PhotoImage(file=asset_path + '/gold_GS.png'),
+        'BGS'   : ImageTk.PhotoImage(file=asset_path + '/gold_BS.png')
+    }
+
+    # Draw Background
+    for y, item in enumerate(maze):
+        print(y, item)
+        for x, tile in enumerate(item):
+            print(x, tile, background[tile])
+            photo = background[tile]
+            canvas.image = photo
+            canvas.create_image((x * background[tile].width(), y * background[tile].height()), anchor = 'nw', image=photo)
+            #if 'S': canvas.create_image(x * background[tile].width(), y * background[tile].height(), anchor = 'nw', image=background['S'])
+            #elif 'A': canvas.create_image(x * background[tile].width(), y * background[tile].height(), anchor = 'nw', image=background['A'])
+            #elif 'P': canvas.create_image(x * background[tile].width(), y * background[tile].height(), anchor = 'nw', image=background['P'])
+            #elif '-': canvas.create_image(x * background[tile].width(), y * background[tile].height(), anchor = 'nw', image=background['-'])
+            #elif 'B': canvas.create_image(x * background[tile].width(), y * background[tile].height(), anchor = 'nw', image=background['B'])
+            #elif 'W': canvas.create_image(x * background[tile].width(), y * background[tile].height(), anchor = 'nw', image=background['W'])
+            #elif 'G': canvas.create_image(x * background[tile].width(), y * background[tile].height(), anchor = 'nw', image=background['G'])
+            #elif 'BS': canvas.create_image(x * background[tile].width(), y * background[tile].height(), anchor = 'nw', image=background['BS'])
+            #elif 'BG': canvas.create_image(x * background[tile].width(), y * background[tile].height(), anchor = 'nw', image=background['BG'])
+            #elif 'GS': canvas.create_image(x * background[tile].width(), y * background[tile].height(), anchor = 'nw', image=background['GS'])
+            #elif 'BGS': canvas.create_image(x * background[tile].width(), y * background[tile].height(), anchor = 'nw', image=background['BGS'])
 
 if __name__ == '__main__':
     filename = argv[1]
     Map_path = '../../INPUT/MAP/'
-    asset_path = '../../ASSET/'
+    asset_path = '../../ASSET'
     
-    map_size, maze = handlein.read_file(filename)
-
+    map_size, maze = handlein.read_file(Map_path + filename)
+    
     for each in maze:
         print(each)
     
     root = Tk()
-    canvas = Canvas(root)
-    canvas.pack()
+    canvas = Canvas(root, width='400p', height='400p')
+    canvas.pack(expand=YES, fill=BOTH)
     root.title("WUMPUS WORLD AI")
-    icon = ImageTk.PhotoImage(Image.open(asset_path + 'Wumpus-Icon.png'))
+    icon = ImageTk.PhotoImage(Image.open(asset_path + '/Wumpus-Icon.png'))
     root.iconphoto(None, icon)
     DrawMap(canvas, maze, map_size, asset_path)
     root.mainloop()
-
-    
