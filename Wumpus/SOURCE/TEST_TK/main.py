70ea5c1796944a14f29ada47c95c6905a1adbe1e
HiraiMomo-TWICE <caogiahung11A2@gmail.com>
2020-08-29 15:26:10 +0700

    update

--- Wumpus/SOURCE/TEST_TK/main.py
+++ Wumpus/SOURCE/TEST_TK/main.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
     icon = ImageTk.PhotoImage(Image.open(asset_path + '/Wumpus-Icon.png'))
     root.iconphoto(None, icon)
     
-
+    # Draw Background
     background = {
         '-'     : ImageTk.PhotoImage(file=asset_path + '/terrain.png'),
         'P'     : ImageTk.PhotoImage(file=asset_path + '/pit.png'),
@@ -35,32 +35,64 @@ if __name__ == '__main__':
         'BGS'   : ImageTk.PhotoImage(file=asset_path + '/terrain_BS.png')
     }
 
+    # Game object
     game_object = {
         'G'     : ImageTk.PhotoImage(file=asset_path + '/gold.png'),
         'W'     : ImageTk.PhotoImage(file=asset_path + '/wumpus.png'),
         'A'     : ImageTk.PhotoImage(file=asset_path + '/agent_right.png')
     }
 
+    # Agent Direction
+    agent_direction = {
+        'UP'    : ImageTk.PhotoImage(file=asset_path + '/agent_up.png'),
+        'RIGHT' : ImageTk.PhotoImage(file=asset_path + '/agent_right.png'),
+        'DOWN'  : ImageTk.PhotoImage(file=asset_path + '/agent_down.png'),
+        'LEFT'  : ImageTk.PhotoImage(file=asset_path + '/agent_right.png'),
+    }
+    
+    # Use to cover unvistited position
     hidden = ImageTk.PhotoImage(file=asset_path + '/hidden.png')
 
+    # Draw Maze
     for y, item in enumerate(maze):
         for x, tile in enumerate(item):
             canvas.create_image((x * background[tile].width(), y * background[tile].height()), anchor = 'nw', image=background[tile])
 
+    # Put Object
     for y, item in enumerate(maze):
         for x, tile in enumerate(item):
             if 'G' in tile:
                 canvas.create_image((x * game_object['G'].width(), y * game_object['G'].height()), anchor = 'nw', image=game_object['G'])
             elif 'W' in tile:
                 canvas.create_image((x * game_object['W'].width(), y * game_object['W'].height()), anchor = 'nw', image=game_object['W'])
-            elif 'A' in tile:
-                canvas.create_image((x * game_object['A'].width(), y * game_object['A'].height()), anchor = 'nw', image=game_object['A'])
             else:
                 continue
 
+    #######################################################################################################################################       
+
+    # Initialize the game
     for y, item in enumerate(maze):
         for x, tile in enumerate(item):
             if 'A' not in tile:
                 canvas.create_image((x * hidden.width(), y * hidden.height()), anchor = 'nw', image=hidden)
+            else:
+                canvas.create_image((x * game_object['A'].width(), y * game_object['A'].height()), anchor = 'nw', image=game_object['A'])
+            
+    # Game
+    score = 0
+    starting_position = tuple() # A position to escape
+    for y, item in enumerate(maze):
+        for x, tile in enumerate(item):
+            if 'A' in tile:
+                starting_position = (x, y)
+                break
+
+    
+
+
+
+
+
+    
             
     root.mainloop()
